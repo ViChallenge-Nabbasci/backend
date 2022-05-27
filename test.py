1db16c49b7204305120ddf5e41632238d2642fbc
chrg <christian.ricci2000@gmail.com>
2022-05-27 15:34:02 +0200

    add /getUser

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel, Field
 import socket
 
@@ -9,25 +9,32 @@ def _find_next_id():
     return max(User.id for us in users) + 1
 
 class User(BaseModel):
-    id: int = Field(default_factory=_find_next_id, alias="id")
+    # id: int = Field(default_factory=_find_next_id, alias="id")
     username: str
+    email: str
 
 users = [
-    User(id=1, username="flanny"),
-    User(id=2, username="chrg127"),
-    User(id=3, username="federaffo00"),
+    User(username="flanny", email="a@b.org"),
+    User(username="chrg127", email="c@d.org"),
+    User(username="federaffo00", email="e@f.org"),
 ]
 
 @app.get("/getUsers")
-async def get_countries():
+async def get_users():
     return users
 
 @app.post("/addUser", status_code=201)
-async def add_country(newUser: User):
+async def add_user(newUser: User):
     users.append(newUser)
     return users
 
+class GetUserRequest(BaseModel):
+    email: str
 
+@app.post("/getUser")
+async def get_user(req: GetUserRequest):
+    r = [ u for u in users if u.email == req.email ]
+    return {"response":"bad id"} if len(r) == 0 else r[0]
 
 hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
